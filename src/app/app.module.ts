9ee4d36ab3ae908269c0880ffcce157bba0242a3
Anton Rechenauer <antonrechenauer@gmail.com>
2016-09-16 15:03:18 +0200

    immer noch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LeftComponent } from './left/left.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+import { ControlComponent } from './control/control.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LeftComponent,
+    ContentComponent,
+    FooterComponent,
+    ControlComponent
   ],
   imports: [
     BrowserModule,
